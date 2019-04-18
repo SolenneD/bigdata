e5b540a13858477608e34868260e6078eb3eb9bb
SolenneD <sdaguerre75@gmail.com>
2019-04-18 20:12:53 +0200

    bla

--- insert.js
+++ insert.js
@@ -1,18 +1,18 @@
-let MongoClient = require('mongodb').MongoClient
-let url = "mongodb://localhost:27017/"
+// let MongoClient = require('mongodb').MongoClient
+// let url = "mongodb://localhost:27017/"
 
-const csvFilePath='./csv/output-0.csv'
-const csv=require('csvtojson')
+// const csvFilePath='./csv/output-0.csv'
+// const csv=require('csvtojson')
 
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-	MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
-		if (err) throw err;
-		var dbo = db.db('bg');
-		dbo.collection('etablissements').insertMany(jsonObj, (err, res) => {
-		if (err) throw err;
-		db.close();
-		});
-	});
-})
+// csv()
+// .fromFile(csvFilePath)
+// .then((jsonObj)=>{
+// 	MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+// 		if (err) throw err;
+// 		var dbo = db.db('bg');
+// 		dbo.collection('etablissements').insertMany(jsonObj, (err, res) => {
+// 		if (err) throw err;
+// 		db.close();
+// 		});
+// 	});
+// })
--- process.js
+++ process.js
@@ -1,29 +1,30 @@
 const fs = require("fs")
 const pm2 = require('pm2')
 
+  pm2.connect(function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
+  });
 
-pm2.connect(function(err) {
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
+for(let i = 0 ; i<4 ; i++) {
+  pm2.start({
+    script    : 'app.js',
+  }, function(err, apps) {
+      console.log(i)
+       pm2.sendDataToProcessId({
+        type: 'process:msg',
+        data: 'bluh',
+        topic: 'bluh',
+        id: i
+     }, function(err, res) {
+        if (err)
+         console.log(err)
+   });
+  });
   
-  
-});
-pm2.start({
-  script    : 'app.js',
-}, function(err, apps) {
-     pm2.sendDataToProcessId({
-      type: 'process:msg',
-      data: 'toto',
-      topic: 'toto',
-      id: 0
-   }, function(err, res) {
-      if (err)
-       throw err;
- });
-  if (err) throw err
-});
+  // pm2.delete(0)
+  // pm2.disconnect();
 
-// pm2.delete(0)
-// pm2.disconnect();
+}
